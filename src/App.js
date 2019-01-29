57242236673d992b4fe3d9e1aeebb73d6f556973
Oscar Sánchez <osanchez@distritomoda.com.ar>
2019-01-29 08:35:59 -0300

    adding libraries

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Container } from 'semantic-ui-react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import HeaderApp from './components/Header'
+
+export default class App extends Component {
+	render() {
+		return (
+			<Container>
+				<HeaderApp></HeaderApp>
+		    <p>
+		      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+		      Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
+		      ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+		      consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+		      In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+		      link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean
+		      vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
+		      enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla
+		      ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.
+		      Curabitur ullamcorper ultricies nisi.
+		    </p>
+  		</Container>
+		)
+	}
 }
-
-export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Header } from 'semantic-ui-react'
+
+export default class HeaderApp extends Component {
+	render() {
+		return (
+			<Header inverted size='huge'>Huge Header</Header>
+		)
+	}
+}
